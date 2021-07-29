fdd0651047bde70de3e70d87c237aa28979bf257
Ole-Christoffer Granmo <ole.granmo@uia.no>
2021-07-29 16:42:00 +0200

    Update CIFARDemo.py

--- examples/CIFARDemo.py
+++ examples/CIFARDemo.py
@@ -1,4 +1,4 @@
-from PyTsetlinMachineCUDA.tm import MultiClassConvolutionalTsetlinMachine2D
+from PyCoalescedTsetlinMachineCUDA.tm import MultiClassConvolutionalTsetlinMachine2D
 import numpy as np
 from time import time
 
