5dbad83fb0742a8e9f5c1ce30e904a380875c085
Ole-Christoffer Granmo <ole.granmo@uia.no>
2021-07-29 17:02:24 +0200

    Update tm.py

--- PyCoalescedTsetlinMachineCUDA/tm.py
+++ PyCoalescedTsetlinMachineCUDA/tm.py
@@ -23,7 +23,7 @@
 
 import numpy as np
 
-import PyTsetlinMachineCUDA.kernels as kernels
+import PyCoalescedTsetlinMachineCUDA.kernels as kernels
 
 import pycuda.curandom as curandom
 import pycuda.driver as cuda
