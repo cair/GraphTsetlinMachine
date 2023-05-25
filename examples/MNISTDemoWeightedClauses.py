e78da4946a5ea837ab623f3f438f72ed1510c4d5
Ole-Christoffer Granmo <ole.granmo@uia.no>
2023-05-25 11:59:20 +0200

    Sparse Coalesced CUDA

--- examples/MNISTDemoWeightedClauses.py
+++ examples/MNISTDemoWeightedClauses.py
@@ -1,4 +1,4 @@
-from PySparseCoalescedTsetlinMachineCUDA.tm import MultiClassTsetlinMachine
+from PyCoalescedTsetlinMachineCUDA.tm import MultiClassTsetlinMachine
 import numpy as np
 from time import time
 
