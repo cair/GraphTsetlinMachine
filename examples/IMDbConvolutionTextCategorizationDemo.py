e762e3372cb7fb3d9b9f4f2ceb549bf6f0d0fdd9
Ole-Christoffer Granmo <ole.granmo@uia.no>
2023-05-25 17:30:56 +0200

    Sparse Coalesced CUDA

--- examples/IMDbConvolutionTextCategorizationDemo.py
+++ examples/IMDbConvolutionTextCategorizationDemo.py
@@ -53,7 +53,7 @@ print(train_y.shape[0])
 X_train = lil_matrix((train_y.shape[0], maxlen*(NUM_WORDS+INDEX_FROM)), dtype=np.uint32)
 for e in range(train_y.shape[0]):
 	position = 0
-	for word_id in train_x[i]:
+	for word_id in train_x[e]:
 		X_train[e, position*(NUM_WORDS+INDEX_FROM) + word_id] = 1
 X_train = X_train.tocsr()
 
@@ -61,7 +61,7 @@ print(test_y.shape[0])
 X_test = lil_matrix((test_y.shape[0], maxlen*(NUM_WORDS+INDEX_FROM)), dtype=np.uint32)
 for e in range(test_y.shape[0]):
 	position = 0
-	for word_id in test_x[i]:
+	for word_id in test_x[e]:
 		X_test[e, position*(NUM_WORDS+INDEX_FROM) + word_id] = 1
 X_test = X_test.tocsr()
 
