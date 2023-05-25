0302110f34ff81c542c2e6f981233d40f9f8baa1
Ole-Christoffer Granmo <ole.granmo@uia.no>
2023-05-25 11:52:24 +0200

    Sparse Coalesced CUDA

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import *
 
 setup(
-   name='PyCoalescedTsetlinMachineCUDA',
+   name='PySparseCoalescedTsetlinMachineCUDA',
    version='0.1.9',
    author='Ole-Christoffer Granmo',
    author_email='ole.granmo@uia.no',
-   url='https://github.com/cair/pyCoalescedTsetlinMachineCUDA/',
+   url='https://github.com/cair/PySparseCoalescedTsetlinMachineCUDA/',
    license='MIT',
    description='Coalesced Tsetlin Machine Architecture.',
    long_description='Coalesced Tsetlin Machine Architecture.',
    keywords ='pattern-recognition cuda machine-learning interpretable-machine-learning rule-based-machine-learning propositional-logic tsetlin-machine regression convolution classification multi-layer',
-   packages=['PyCoalescedTsetlinMachineCUDA']
+   packages=['PySparseCoalescedTsetlinMachineCUDA']
 )
