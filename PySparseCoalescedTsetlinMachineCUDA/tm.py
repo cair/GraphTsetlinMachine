733a58acd32d00ea2cdbf58e7d00e831d19184ea
Ole-Christoffer Granmo <ole.granmo@uia.no>
2023-05-25 14:21:37 +0200

    Sparse Coalesced CUDA

--- PySparseCoalescedTsetlinMachineCUDA/tm.py
+++ PySparseCoalescedTsetlinMachineCUDA/tm.py
@@ -159,23 +159,6 @@ class CommonTsetlinMachine():
 
 		print(self.number_of_outputs, self.number_of_clauses, self.number_of_features, self.number_of_state_bits, self.boost_true_positive_feedback, self.s, self.T, self.q, self.negative_clauses, self.number_of_patches, X.shape[0])
 
-		parameters = """
-#define CLASSES %d
-#define CLAUSES %d
-#define FEATURES %d
-#define STATE_BITS %d
-#define BOOST_TRUE_POSITIVE_FEEDBACK %d
-#define S %f
-#define THRESHOLD %d
-#define Q %f
-
-#define NEGATIVE_CLAUSES %d
-
-#define PATCHES %d
-
-#define NUMBER_OF_EXAMPLES %d
-""" % (self.number_of_outputs, self.number_of_clauses, self.number_of_features, self.number_of_state_bits, self.boost_true_positive_feedback, self.s, self.T, self.q, self.negative_clauses, self.number_of_patches, X.shape[0])
-
 		mod_prepare = SourceModule(parameters + kernels.code_header + kernels.code_prepare, no_extern_c=True)
 		self.prepare = mod_prepare.get_function("prepare")
 
