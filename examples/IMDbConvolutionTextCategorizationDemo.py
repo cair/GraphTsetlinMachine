49dc2f426ee46bb462ac84530792cc7be7ebac0e
Ole-Christoffer Granmo <ole.granmo@uia.no>
2023-05-25 17:33:28 +0200

    Sparse Coalesced CUDA

--- examples/IMDbConvolutionTextCategorizationDemo.py
+++ examples/IMDbConvolutionTextCategorizationDemo.py
@@ -71,13 +71,13 @@ tm = MultiClassConvolutionalTsetlinMachine2D(clauses, T, s, (maxlen, 1, (NUM_WOR
 
 for i in range(epochs):
     start_training = time()
-    tm.fit(X_train, Y_train, epochs=1, incremental=True)
+    tm.fit(X_train, train_y, epochs=1, incremental=True)
     stop_training = time()
 
     start_testing = time()
-    result_test = 100*(tm.predict(X_test) == Y_test).mean()
+    result_test = 100*(tm.predict(X_test) == test_y).mean()
     stop_testing = time()
 
-    result_train = 100*(tm.predict(X_train) == Y_train).mean()
+    result_train = 100*(tm.predict(X_train) == train_y).mean()
 
     print("#%d Accuracy Test: %.2f%% Accuracy Train: %.2f%% Training: %.2fs Testing: %.2fs" % (i+1, result_test, result_train, stop_training-start_training, stop_testing-start_testing))
