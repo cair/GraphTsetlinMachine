ba12f078ce65cda296a8719d0dd529333b612e0d
Ole-Christoffer Granmo <ole.granmo@uia.no>
2023-05-25 19:53:20 +0200

    Sparse Coalesced CUDA

--- examples/IMDbConvolutionTextCategorizationDemo.py
+++ examples/IMDbConvolutionTextCategorizationDemo.py
@@ -11,7 +11,7 @@ from scipy.sparse import csr_matrix, csc_matrix, lil_matrix
 
 from PySparseCoalescedTsetlinMachineCUDA.tm import MultiClassConvolutionalTsetlinMachine2D, MultiClassTsetlinMachine
 
-maxlen = 50
+maxlen = 250
 
 epochs = 100
 
