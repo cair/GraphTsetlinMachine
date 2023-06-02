43b9e212379873d181cc05900ad9f4e5d0e53bd0
Ole-Christoffer Granmo <ole.granmo@uia.no>
2023-06-02 11:56:37 +0200

    Update

--- examples/CIFAR2ColorPaletteHypervector.py
+++ examples/CIFAR2ColorPaletteHypervector.py
@@ -2,6 +2,7 @@ import numpy as np
 from time import time
 from PySparseCoalescedTsetlinMachineCUDA.tm import MultiClassConvolutionalTsetlinMachine2D
 from scipy.sparse import csr_matrix, csc_matrix, lil_matrix
+from numba import jit
 
 import ssl
 
@@ -10,7 +11,7 @@ from keras.datasets import cifar10
 
 scaling = 1.0
 
-hypervector_size = 256
+hypervector_size = 64
 
 bits = 5
 
@@ -21,17 +22,34 @@ animals = np.array([2, 3, 4, 5, 6, 7])
 ensembles = 5
 epochs = 250
 
-examples = 5000
+examples = 1000
 
 max_included_literals = 32
 
-clauses = 8000*2
-T = int(clauses * 0.75)
+#clauses = 8000*2
+#T = int(clauses * 0.75)
 
-#clauses = 2000
-#T = 5000
+clauses = 2000
+T = 5000
 
-s = 10.0
+s = 1.5
+
+@jit(nopython=True)
+def produce_hypervector(hypervector, encoding, X, i, x, y):
+        hypervector[:] = 0
+
+        roll = 0
+        for c in range(3):
+                if c == 0:
+                        roll += (X[i, x, y, c] // 32)
+                elif c == 1:
+                        roll += 11 * (X[i, x, y, c] // 32)
+                else:
+                        code = encoding[(X[i, x, y, c] // 32)]
+                        for bit in code:
+                                hypervector[((bit + roll) % hypervector_size)] = 1
+
+        return
 
 indexes = np.arange(hypervector_size, dtype=np.uint32)
 encoding = np.zeros((resolution, bits), dtype=np.uint32)
@@ -39,28 +57,23 @@ for i in range(resolution):
         encoding[i] = np.random.choice(indexes, size=(bits))
 
 (X_train_org, Y_train), (X_test_org, Y_test) = cifar10.load_data()
-X_train_org = X_train_org#[0:examples]
-X_test_org = X_test_org#[0:examples]
-Y_train = Y_train.reshape(Y_train.shape[0])#[0:examples]
-Y_test = Y_test.reshape(Y_test.shape[0])#[0:examples]
+X_train_org = X_train_org[0:examples]
+X_test_org = X_test_org[0:examples]
+Y_train = Y_train.reshape(Y_train.shape[0])[0:examples]
+Y_test = Y_test.reshape(Y_test.shape[0])[0:examples]
 
 Y_train = np.where(np.isin(Y_train, animals), 1, 0)
 Y_test = np.where(np.isin(Y_test, animals), 1, 0)
+
+hypervector = np.zeros(hypervector_size, dtype=np.uint32)
 
 X_train = lil_matrix((X_train_org.shape[0], X_train_org.shape[1] * X_train_org.shape[2] * hypervector_size), dtype=np.uint32)
 for i in range(X_train.shape[0]):
         for x in range(X_train_org.shape[1]):
                 for y in range(X_train_org.shape[2]):
-                        roll = 0
-                        for c in range(X_train_org.shape[3]):
-                                if c == 0:
-                                        roll += (X_train_org[i, x, y, c] // 32)
-                                elif c == 1:
-                                        roll += 11 * (X_train_org[i, x, y, c] // 32)
-                                else:
-                                        code = encoding[(X_train_org[i, x, y, c] // 32)]
-                                        for bit in code:
-                                                X_train[i, x * X_train_org.shape[2] * hypervector_size + y * hypervector_size + ((bit + roll) % hypervector_size)] = 1
+                        produce_hypervector(hypervector, encoding, X_train_org, i, x, y)
+                        for bit in hypervector.nonzero():
+                                X_train[i, x * X_train_org.shape[2] * hypervector_size + y * hypervector_size + bit] = 1
 
 X_test = lil_matrix((X_test_org.shape[0], X_test_org.shape[1] * X_test_org.shape[2] * hypervector_size), dtype=np.uint32)
 for i in range(X_test.shape[0]):
