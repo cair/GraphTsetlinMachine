cadcee1c9c571425f9988d3e5df47128b293a628
Ole-Christoffer Granmo <ole.granmo@uia.no>
2023-06-26 17:13:19 +0200

    CIFAR Multioutput

--- examples/CIFARMultiOutput.py
+++ examples/CIFARMultiOutput.py
@@ -17,31 +17,39 @@ scaling = 1.0
 
 resolution = 8
 
-animals = np.array([2, 3, 4, 5, 6, 7])
-random_grouping = np.random.choice(10, size=5, replace=False)
+groups = 10
 
 ensembles = 5
 epochs = 250
 
 max_included_literals = 32
-clauses = 2000
-T = 5000
+clauses = 2000//10
+T = 5000//10
 s = 1.5
 
+clauses_2 = 2000
+T_2 = 5000
+s_2 = 1.5
+
 (X_train_org, Y_train_org), (X_test_org, Y_test_org) = cifar10.load_data()
-X_train_org = X_train_org[0:5000]
-X_test_org = X_test_org[0:5000]
+X_train_org = X_train_org[0:100]
+X_test_org = X_test_org[0:100]
 
-Y_train_org = Y_train_org.reshape(Y_train_org.shape[0])[0:5000]
-Y_test_org = Y_test_org.reshape(Y_test_org.shape[0])[0:5000]
+Y_train_org = Y_train_org.reshape(Y_train_org.shape[0])[0:100]
+Y_test_org = Y_test_org.reshape(Y_test_org.shape[0])[0:100]
 
-Y_train = np.empty((Y_train_org.shape[0], 2), dtype=np.uint32)
-Y_train[:, 0] = np.where(np.isin(Y_train_org, animals), 1, 0)
-Y_train[:, 1] = np.where(np.isin(Y_train_org, random_grouping), 1, 0)
+X_train = np.where(X_train.reshape((X_train.shape[0], 28*28)) > 75, 1, 0) 
+X_test = np.where(X_test.reshape((X_test.shape[0], 28*28)) > 75, 1, 0) 
 
-Y_test = np.empty((Y_test_org.shape[0], 2), dtype=np.uint32)
-Y_test[:, 0] = np.where(np.isin(Y_test_org, animals), 1, 0)
-Y_test[:, 1] = np.where(np.isin(Y_test_org, random_grouping), 1, 0)
+Y_train = np.empty((Y_train_org.shape[0], groups), dtype=np.uint32)
+random_grouping = []
+for group in range(groups):
+        random_grouping.append(np.random.choice(10, size=5, replace=False))
+        Y_train[:, group] = np.where(np.isin(Y_train_org, random_grouping[-1]), 1, 0)
+
+Y_test = np.empty((Y_test_org.shape[0], groups), dtype=np.uint32)
+for group in range(groups):
+        Y_test[:, group] = np.where(np.isin(Y_test_org, random_grouping[group]), 1, 0)
 
 X_train = np.empty((X_train_org.shape[0], X_train_org.shape[1], X_train_org.shape[2], X_train_org.shape[3], resolution),
                    dtype=np.uint8)
@@ -88,3 +96,20 @@ for ensemble in range(ensembles):
                 print("%d %d %.2f %.2f %.2f %.2f %.2f" % (ensemble, epoch, number_of_includes, result_test, result_train, stop_training-start_training, stop_testing-start_testing), file=f)
                 f.flush()
 f.close()
+
+X_train_transformed = tm.transform(X_train)
+X_test_transformed = tm.transform(X_test)
+
+tm = MultiClassTsetlinMachine(clauses_2, T_2, s)
+for i in range(epochs):
+        start_training = time()
+        tm.fit(X_train_transformed, Y_train_org, epochs=1, incremental=True)
+        stop_training = time()
+
+        start_testing = time()
+        result_test = 100*(tm.predict(X_test_transformed) == Y_test_org).mean()
+        stop_testing = time()
+
+        result_train = 100*(tm.predict(X_train_transformed) == Y_train_org).mean()
+
+        print("#%d Accuracy Test: %.2f%% Accuracy Train: %.2f%% Training: %.2fs Testing: %.2fs" % (i+1, result_test, result_train, stop_training-start_training, stop_testing-start_testing))
