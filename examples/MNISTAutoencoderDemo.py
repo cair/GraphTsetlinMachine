fe83f94df18f944c51eeb7759db28d0fa25df7b8
Ole-Christoffer Granmo <ole.granmo@uia.no>
2023-07-15 00:49:55 +0200

    MNIST Autoencoder Demo

--- examples/MNISTAutoencoderDemo.py
+++ examples/MNISTAutoencoderDemo.py
@@ -35,8 +35,8 @@ output_active = np.arange(number_of_features, dtype=np.uint32)
 X_train_org = X_train_org.reshape((X_train_org.shape[0], -1))
 X_test_org = X_test_org.reshape((X_test_org.shape[0], -1))
 
-X_train = np.zeros((X_train_org.shape[0], 28*28 + 10), dtype=np.uint32)
-X_test = np.zeros((X_test_org.shape[0], 28*28 + 10), dtype=np.uint32)
+X_train = np.zeros((X_train_org.shape[0], 28*28 + 20), dtype=np.uint32)
+X_test = np.zeros((X_test_org.shape[0], 28*28 + 20), dtype=np.uint32)
 
 X_train[:,:28*28] = np.where(X_train_org > 75, 1, 0)
 X_test[:,:28*28] = np.where(X_test_org > 75, 1, 0)
@@ -44,6 +44,8 @@ X_test[:,:28*28] = np.where(X_test_org > 75, 1, 0)
 for i in range(10):
 	X_train[:,28*28 + i] = (i == Y_train)
 	X_test[:,28*28 + i] = (i == Y_test)
+	X_train[:,28*28 + 10 + i] = (i == Y_train)
+	X_test[:,28*28 + 10 + i] = (i == Y_test)
 
 #X_train_noisy = np.where(np.random.rand(X_train.shape[0], number_of_features) <= noise, 1-X_train, X_train) # Adds noise
 
@@ -79,17 +81,17 @@ for e in range(100):
 
 	print("\nTraining Time: %.2f" % (stop_training - start_training))
 
-	tm_2 = MultiClassTsetlinMachine(2000, 50*100, 10.0)
+	# tm_2 = MultiClassTsetlinMachine(2000, 50*100, 10.0)
 
-	print("\n\tAccuracy over 10 epochs:\n")
-	for i in range(3):
-		start_training = time()
-		tm_2.fit(X_train_predicted, Y_train, epochs=1, incremental=True)
-		stop_training = time()
+	# print("\n\tAccuracy over 10 epochs:\n")
+	# for i in range(3):
+	# 	start_training = time()
+	# 	tm_2.fit(X_train_predicted, Y_train, epochs=1, incremental=True)
+	# 	stop_training = time()
 
-		start_testing = time()
-		result = 100*(tm_2.predict(X_test_predicted) == Y_test).mean()
-		stop_testing = time()
+	# 	start_testing = time()
+	# 	result = 100*(tm_2.predict(X_test_predicted) == Y_test).mean()
+	# 	stop_testing = time()
 
-		print("\t#%d Accuracy: %.2f%% Training: %.2fs Testing: %.2fs" % (i+1, result, stop_training-start_training, stop_testing-start_testing))
+	# 	print("\t#%d Accuracy: %.2f%% Training: %.2fs Testing: %.2fs" % (i+1, result, stop_training-start_training, stop_testing-start_testing))
 
