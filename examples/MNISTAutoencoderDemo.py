d533c7543213a766d1d8c08b080babb8e60aabf7
Ole-Christoffer Granmo <ole.granmo@uia.no>
2023-07-14 23:05:13 +0200

    MNIST Autoencoder Demo

--- examples/MNISTAutoencoderDemo.py
+++ examples/MNISTAutoencoderDemo.py
@@ -28,15 +28,23 @@ print("Number of features:", number_of_features)
 
 output_active = np.arange(number_of_features, dtype=np.uint32)
 
-(X_train, Y_train), (X_test, Y_test) = mnist.load_data()
+(X_train_org, Y_train), (X_test_org, Y_test) = mnist.load_data()
 
-X_train = np.where(X_train > 75, 1, 0)
-X_test = np.where(X_test > 75, 1, 0)
+X_train_org = X_train_org.reshape((X_train.shape[0], -1))
+X_test_org = X_test_org.reshape((X_test.shape[0], -1))
 
-X_train = X_train.reshape((X_train.shape[0], -1))
-X_train_noisy = np.where(np.random.rand(X_train.shape[0], number_of_features) <= noise, 1-X_train, X_train) # Adds noise
+X_train = np.zeros((X_train_org.shape[0], 28*28 + 10), dtype=np.uint32)
+X_test = np.zeros((X_test_org.shape[0], 28*28 + 10), dtype=np.uint32)
 
-X_test = X_test.reshape((X_test.shape[0], -1))
+X_train[:,:28*28] = np.where(X_train > 75, 1, 0)
+X_test[:,:28*28] = np.where(X_test > 75, 1, 0)
+
+for i in range(10):
+	X_train[:,28*28 + i] = (i == Y_train)
+	X_test[:,28*28 + i] = (i == Y_test)
+
+#X_train_noisy = np.where(np.random.rand(X_train.shape[0], number_of_features) <= noise, 1-X_train, X_train) # Adds noise
+
 
 X_train = csr_matrix(X_train)
 X_test = csr_matrix(X_test)
@@ -45,11 +53,21 @@ tm = MultiOutputConvolutionalTsetlinMachine2D(number_of_clauses, T, s, (28, 28, 
 
 print("\nAccuracy Over 40 Epochs:")
 for e in range(100):
+	print("\nEpoch #%d\n" % (e+1))
+
 	start_training = time()
 	tm.fit(X_train_noisy, X_train.toarray(), epochs=1, incremental=True)
 	stop_training = time()
 
-	print("\nEpoch #%d\n" % (e+1))
+	Y_train_scores tm.score(X_train)
+	Y_test_scores = tm.score(X_test)
+
+	Y_train_predicted = np.argmax(Y_train_scores[:,28*28:], axis=1) 
+	Y_test_predicted = np.argmax(Y_test_scores[:,28*28:], axis=1) 
+
+	print("Classification train accuracy:", 100*(Y_train_predicted == Y_train).mean())
+
+	print("Classification test accuracy:", 100*(Y_test_predicted == Y_test).mean())
 
 	X_test_predicted = tm.predict(X_test)
 	X_train_predicted = tm.predict(X_train)
