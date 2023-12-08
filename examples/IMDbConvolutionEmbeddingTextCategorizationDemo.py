8f7a43247ad8e31a65731cc04528b0323fe6d58d
Ole-Christoffer Granmo <ole.granmo@uia.no>
2023-12-08 12:15:27 +0100

    Update

--- examples/IMDbConvolutionEmbeddingTextCategorizationDemo.py
+++ examples/IMDbConvolutionEmbeddingTextCategorizationDemo.py
@@ -42,15 +42,13 @@ id_to_word = {value:key for key,value in word_to_id.items()}
 
 # Read from file instead, otherwise the same
 
-token_indeks = {}
-
+encoding = {}
 f = open("/data/near-lossless-binarization/binary_vectors_1024.vec", "r")
 s = f.readline().strip()
 while s:
 	entries = s.split(" ")
-	token = entries[0]
 	values = np.unpackbits(np.fromstring(" ".join(entries[1:]), dtype=np.int64, sep=' ').view(np.uint8))
-	print(token, values)
+	encoding[word_to_id[entries[0]]] = np.unpackbits(np.fromstring(" ".join(entries[1:]), dtype=np.int64, sep=' ').view(np.uint8)).nonzero()
 	s = f.readline().strip()
 
 indexes = np.arange(hypervector_size, dtype=np.uint32)
