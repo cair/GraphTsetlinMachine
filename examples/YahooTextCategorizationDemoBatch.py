75ec12d48b5eb92ec0ceb699089a1cbed5d22709
Ole-Christoffer Granmo <ole.granmo@uia.no>
2023-12-15 21:27:29 +0100

    Update

--- examples/YahooTextCategorizationDemoBatch.py
+++ examples/YahooTextCategorizationDemoBatch.py
@@ -69,4 +69,4 @@ for i in range(epochs):
 		result_test = 100*(tm.predict(X_test) == Y_test).mean()
 		stop_testing = time()
 
-		print("#%d Accuracy Test: %.2f%% Accuracy Train: %.2f%% Training: %.2fs Testing: %.2fs" % (i+1, result_test, result_train, stop_training-start_training, stop_testing-start_testing))
+		print("#%d Accuracy Test: %.2f%% Training: %.2fs Testing: %.2fs" % (i+1, result_test, stop_training-start_training, stop_testing-start_testing))
