f4402b573af45132d0dc039802eb93fcc85dec4f
Ole-Christoffer Granmo <ole.granmo@uia.no>
2024-09-08 10:33:48 +0200

    Update

--- examples/MNISTDemo2DConvolutionWeightedClauses.py
+++ examples/MNISTDemo2DConvolutionWeightedClauses.py
@@ -1,4 +1,4 @@
-from PySparseCoalescedTsetlinMachineCUDA.tm import MultiClassConvolutionalTsetlinMachine2D
+from GraphTsetlinMachine import MultiClassConvolutionalTsetlinMachine2D
 
 import numpy as np
 from time import time
