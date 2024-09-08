d2067a5703ad0ea4d7062726b79d69f3c62ffaef
Ole-Christoffer Granmo <ole.granmo@uia.no>
2024-09-08 15:52:06 +0200

    Update

--- examples/SimpleGraphDemo.py
+++ examples/SimpleGraphDemo.py
@@ -3,9 +3,9 @@ import numpy as np
 
 number_of_training_examples = 10000
 
-max_sequence_length = 5
+max_sequence_length = 10
 
-max_number_of_classes = 2 # Must be less than or equal to max sequence length
+max_number_of_classes = 3 # Must be less than or equal to max sequence length
 
 training_examples = []
 
@@ -27,9 +27,9 @@ for i in range(number_of_training_examples):
 
     # Add features
     target = np.random.randint(max_number_of_classes) 
-    position = np.random.randint(max_sequence_length-target)
+    position = np.random.randint(max_sequence_length-target-1)
     print(target, position, max_sequence_length)
-    for p in range(position, position+target):
+    for p in range(position, position+target+1):
         sequence_graph.add_node_feature(p, 'A')
 
     print(sequence_graph.node_name)
