31c53b0cdad2b7b0f6e2fabb55355148c4ec0f5e
Ole-Christoffer Granmo <ole.granmo@uia.no>
2024-09-10 10:44:27 +0200

    Update

--- GraphTsetlinMachine/graph.py
+++ GraphTsetlinMachine/graph.py
@@ -22,17 +22,29 @@ import numpy as np
 
 class Graph():
 	def __init__(self):
-		self.node_name = {}
+		self.node_name_id = {}
+		self.node_id_name = {}
 		self.node_edges = {}
 		self.node_features = {}
+		self.edge_type_id = {}		
+		self.node_counter = 0
+		self.edge_counter = 0
+		self.edge_type_counter = 0
 
 	def add_node(self, node_name):
-		self.node_name[node_name] = 1
+		self.node_name_id[node_name] = self.node_counter
+		self.node_id_name[self.node_counter] = node_name
 		self.node_features[node_name] = {}
 		self.node_edges[node_name] = {}
+		self.node_counter += 1
 
 	def add_edge(self, node_name_1, node_name_2, edge_type='p'):
+		if edge_type not in self.edge_type_id:
+			self.edge_type_id[edge_type] = self.edge_type_counter
+			self.edge_type_counter += 1
+
 		self.node_edges[node_name_1][(node_name_2, edge_type)] = 1
+		self.edge_counter += 1
 
 	def add_node_feature(self, node_name, symbols):
 		if type(symbols) != tuple:
@@ -41,12 +53,16 @@ class Graph():
 		self.node_features[node_name][symbols] = 1
 
 	def encode(self, hypervectors, hypervector_size=1024, hypervector_bits=3):
-		# Vector for encoding graph
-		Xi = np.zeros((len(self.node_name), hypervector_size), dtype=np.uint32)
+		# Vector for encoding node features
+		Nf = np.zeros((len(self.node_name_id), hypervector_size), dtype=np.uint32)
+
+		# Vector for encoding node edges
+		Ne = np.zeros(len(self.node_name_id) + self.edge_counter*2, dtype=np.uint32)
 
 		position = 0
-		for n in self.node_name.keys():
-			for symbols in self.node_features[n].keys():
+		for i in range(self.node_counter):
+			# Encodes node features
+			for symbols in self.node_features[self.node_id_name[i]]:
 				for symbol in symbols:
 					if symbol not in hypervectors:
 						indexes = np.arange(hypervector_size, dtype=np.uint32)
@@ -54,8 +70,16 @@ class Graph():
 						print(symbol, hypervectors[symbol])
 
 				base_indexes = hypervectors[symbols[0]]
-				for i in range(1, len(symbols)):
-					base_indexes = (base_indexes + (hypervectors[symbols[i]][0]+2)*i) % hypervector_size
-				Xi[position][base_indexes] = 1
+				for j in range(1, len(symbols)):
+					base_indexes = (base_indexes + (hypervectors[symbols[j]][0]+2)*j) % hypervector_size
+				Nf[i][base_indexes] = 1
+
+			# Encodes node edges
+			Ne[position] = len(self.node_edges[self.node_id_name[i]])
 			position += 1
-		return Xi
+			for edge in self.node_edges[self.node_id_name[i]]:
+				Ne[position] = self.node_name_id[edge[0]]
+				Ne[position+1] = self.edge_type_id[edge[1]]
+				position += 2
+
+		return (Nf, Ne)
