c1d810dd1d8aa4130f11706b838bad657e7f9dee
Ole-Christoffer Granmo <ole.granmo@uia.no>
2024-09-10 11:12:26 +0200

    Update

--- examples/SimpleGraphDemo.py
+++ examples/SimpleGraphDemo.py
@@ -5,7 +5,7 @@ number_of_training_examples = 10000
 
 max_sequence_length = 5
 
-max_number_of_classes = 2 # Must be less than or equal to max sequence length
+number_of_classes = 2 # Must be less than or equal to max sequence length
 
 training_examples = []
 
@@ -13,30 +13,40 @@ X = []
 hypervectors = {}
 for i in range(number_of_training_examples):
     # Create graph
+
+    sequence_length = np.random.randint(number_of_classes, max_sequence_length+1)
+    print(sequence_length)
+
     sequence_graph = Graph()
 
     # Create nodes
-    for j in range(max_sequence_length):
+    for j in range(sequence_length):
         sequence_graph.add_node(j)
-    
+
     # Add edges in both directions
-    for j in range(max_sequence_length):
+    for j in range(sequence_length):
         if j > 0:
             sequence_graph.add_edge(j, j-1, edge_type='left')
 
-        if j < max_sequence_length-1:
+        if j < sequence_length-1:
             sequence_graph.add_edge(j, j+1, edge_type='right')
 
+    # Select class
+    target = np.random.randint(number_of_classes) 
+
     # Add features
-    target = np.random.randint(max_number_of_classes) 
-    position = np.random.randint(max_sequence_length-target-1)
+    if sequence_length-target-1 == 0:
+        position = 0
+    else:
+        position = np.random.randint(sequence_length-target-1)
+
     for p in range(position, position+target+1):
         sequence_graph.add_node_feature(p, 'A')
         sequence_graph.add_node_feature(p, ('A','B'))
         sequence_graph.add_node_feature(p, ('A','B', 'C'))
  
     print(target)
-    print(position, max_sequence_length)
+    print(position, sequence_length, max_sequence_length)
     print(hypervectors)
     X.append(sequence_graph.encode(hypervectors, hypervector_size=16, hypervector_bits=1))
     print(X[-1])
