3a0b9fad88a4410de81c073ad89ce1fccbfad026
Ole-Christoffer Granmo <ole.granmo@uia.no>
2024-09-12 18:14:33 +0200

    Update

--- GraphTsetlinMachine/tm.py
+++ GraphTsetlinMachine/tm.py
@@ -308,12 +308,21 @@ class CommonTsetlinMachine():
 		self._init_fit(graphs, encoded_Y, incremental)
 
 		class_sum = np.zeros(self.number_of_outputs).astype(np.int32)
-
 		for epoch in range(epochs):
 			for e in range(graphs.X.shape[0]):
 				cuda.memcpy_htod(self.class_sum_gpu, class_sum)
 
-				self.encode.prepared_call(self.grid, self.block, self.X_train_indptr_gpu, self.X_train_indices_gpu, self.encoded_X_gpu, np.int32(e), np.int32(self.hypervector_size), np.int32(self.depth), np.int32(self.append_negated))
+				self.encode.prepared_call(
+					self.grid,
+					self.block,
+					self.X_train_indptr_gpu,
+					self.X_train_indices_gpu,
+					self.encoded_X_gpu,
+					np.int32(e),
+					np.int32(self.hypervector_size),
+					np.int32(self.depth),
+					np.int32(self.append_negated)
+				)
 				cuda.Context.synchronize()
 
 				self.evaluate_update.prepared_call(self.grid, self.block, self.ta_state_gpu, self.clause_weights_gpu, self.class_sum_gpu, self.node_count[e], self.encoded_X_gpu)
@@ -374,12 +383,9 @@ class CommonTsetlinMachine():
 				self.X_test_indices_gpu,
 				self.encoded_X_packed_gpu,
 				np.int32(e),
-				np.int32(self.dim[0]),
-				np.int32(self.dim[1]),
-				np.int32(self.dim[2]),
-				np.int32(self.patch_dim[0]),
-				np.int32(self.patch_dim[1]),
-				np.int32(self.append_negated), np.int32(0)
+				np.int32(self.hypervector_size),
+				np.int32(self.depth),
+				np.int32(self.append_negated)
 			)
 			cuda.Context.synchronize()
 
@@ -403,7 +409,6 @@ class CommonTsetlinMachine():
 
 		return class_sum
 	
-
 class MultiClassGraphTsetlinMachine(CommonTsetlinMachine):
 	"""
 	This class ...
