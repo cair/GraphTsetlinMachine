085ac39edd8af01f5ca2021ee08bacdbf4dd5f3b
Ole-Christoffer Granmo <ole.granmo@uia.no>
2024-09-13 10:34:24 +0200

    Update

--- GraphTsetlinMachine/tm.py
+++ GraphTsetlinMachine/tm.py
@@ -412,7 +412,16 @@ class CommonTsetlinMachine():
 			)
 			cuda.Context.synchronize()
 
-			self.restore_packed.prepared_call(self.grid, self.block, self.X_test_indptr_gpu, self.X_test_indices_gpu, self.encoded_X_packed_gpu, np.int32(e), np.int32(self.dim[0]), np.int32(self.dim[1]), np.int32(self.dim[2]), np.int32(self.patch_dim[0]), np.int32(self.patch_dim[1]), np.int32(self.append_negated), np.int32(0))
+			self.restore_packed.prepared_call(
+				self.grid, self.block,
+				self.X_test_indptr_gpu,
+				self.X_test_indices_gpu,
+				self.encoded_X_packed_gpu,
+				np.int32(e),
+				np.int32(self.hypervector_size),
+				np.int32(self.depth),
+				np.int32(self.append_negated)
+			)
 			cuda.Context.synchronize()
 
 			cuda.memcpy_dtoh(class_sum[e,:], self.class_sum_gpu)
