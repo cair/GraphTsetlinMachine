c09e8ff548d03c08986373a375658bae23404acc
Ole-Christoffer Granmo <ole.granmo@uia.no>
2024-09-13 10:41:22 +0200

    Update

--- examples/SimpleGraphDemo.py
+++ examples/SimpleGraphDemo.py
@@ -59,6 +59,10 @@ for i in range(number_of_training_examples):
 
     graphs_train.add(sequence_graph)
 
+print(graphs_train.hypervectors)
+print(graphs_train.edge_type_id)
+print(graphs_train.node_count)
+
 tm = MultiClassGraphTsetlinMachine(100, 1000, 1.0, hypervector_size=16, hypervector_bits=1, depth=1)
 
 for i in range(epochs):
