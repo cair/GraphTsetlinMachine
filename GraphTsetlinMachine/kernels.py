c4311cc2bf0bd20765ca2a07f0cccc62e71c732c
Ole-Christoffer Granmo <ole.granmo@uia.no>
2024-09-13 19:32:51 +0200

    Update

--- GraphTsetlinMachine/kernels.py
+++ GraphTsetlinMachine/kernels.py
@@ -388,7 +388,7 @@ code_prepare = """
 			for (unsigned long long clause = index; clause < CLAUSES; clause += stride) {
 				for (unsigned long long class_id = 0; class_id < CLASSES; ++class_id) {
 					#if NEGATIVE_CLAUSES == 1
-						clause_weights[class_id*CLAUSES + clause] = 1 - 2 * (curand(&localState) % 2);
+						clause_weights[class_id*CLAUSES + clause] = 1 - 2*(clause % CLASSES != class_id); //1 - 2 * (curand(&localState) % 2);
 					#else
 						clause_weights[class_id*CLAUSES + clause] = 1;
 					#endif
@@ -594,7 +594,7 @@ code_encode = """
 			if (index != 0) {
 				return;
 			}
-			
+
 			int number_of_features = hypervector_size * depth;
 
 			int number_of_literals;
