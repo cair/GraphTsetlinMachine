21be01e1bc2455b23b429abd9023a05a78db1989
Ole-Christoffer Granmo <ole.granmo@uia.no>
2024-09-13 20:01:41 +0200

    Update

--- GraphTsetlinMachine/kernels.py
+++ GraphTsetlinMachine/kernels.py
@@ -125,7 +125,7 @@ code_update = """
 						printf("SELECTED\\n");
 					} else if ((curand(localState) % (output_one_nodes_count + 1)) == 0) {
 						*clause_true_node = node;
-						printf("SELECTED\\n")
+						printf("SELECTED\\n");
 					}
 					output_one_nodes_count += 1;
 				}
