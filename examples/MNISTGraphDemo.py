5ae0275cae79abe35276ebdedfd492e81f581135
Ole-Christoffer Granmo <ole.granmo@uia.no>
2024-09-18 11:02:17 +0200

    Update

---
+++ examples/MNISTGraphDemo.py
@@ -0,0 +1,126 @@
+from PySparseCoalescedTsetlinMachineCUDA.graphs import Graphs
+import numpy as np
+from scipy.sparse import csr_matrix
+from PySparseCoalescedTsetlinMachineCUDA.tm import MultiClassGraphTsetlinMachine
+from time import time
+import argparse
+from skimage.util import view_as_windows
+from keras.datasets import mnist
+from numba import jit
+
+(X_train, Y_train), (X_test, Y_test) = mnist.load_data()
+
+X_train = np.where(X_train > 75, 1, 0)
+X_test = np.where(X_test > 75, 1, 0)
+Y_train = Y_train.astype(np.uint32)
+Y_test = Y_test.astype(np.uint32)
+
+def default_args(**kwargs):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--epochs", default=25, type=int)
+    parser.add_argument("--number-of-clauses", default=20000, type=int)
+    parser.add_argument("--T", default=2500, type=int)
+    parser.add_argument("--s", default=1.5, type=float)
+    parser.add_argument("--hypervector_size", default=128, type=int)
+    parser.add_argument("--hypervector_bits", default=2, type=int)
+    parser.add_argument("--max-included-literals", default=32, type=int)
+
+    args = parser.parse_args()
+    for key, value in kwargs.items():
+        if key in args.__dict__:
+            setattr(args, key, value)
+    return args
+
+args = default_args()
+
+symbols = []
+for i in range(100):
+    symbols.append(i)
+
+for i in range(19):
+    symbols.append('c:' + str(i))
+    symbols.append('r:' + str(i))
+
+number_of_train_nodes = np.repeat(19*19, Y_train.shape[0]).astype(np.uint32)
+
+graphs_train = Graphs(number_of_train_nodes, symbols=symbols, hypervector_size=args.hypervector_size, hypervector_bits=args.hypervector_bits)
+for i in range(X_train.shape[0]):
+    if i % 1000 == 0:
+        print(i, X_train.shape[0])
+     
+    windows = view_as_windows(X_train[i,:,:], (10, 10))
+    for q in range(windows.shape[0]):
+            for r in range(windows.shape[1]):
+                node_id = q*19 + r
+                patch = windows[q,r].reshape(-1).astype(np.uint32)
+                for k in patch.nonzero()[0]:
+                    graphs_train.add_node_feature(i, node_id, k)
+                graphs_train.add_node_feature(i, node_id, 'c:'+str(q))
+                graphs_train.add_node_feature(i, node_id, 'r:'+str(r))
+
+graphs_train.encode()
+
+print("Training data produced")
+
+number_of_test_nodes = np.repeat(19*19, Y_test.shape[0]).astype(np.uint32)
+
+graphs_test = Graphs(number_of_test_nodes, init_with=graphs_train)
+for i in range(X_test.shape[0]):
+    if i % 1000 == 0:
+        print(i, X_test.shape[0])
+     
+    windows = view_as_windows(X_test[i,:,:], (10, 10))
+    for q in range(windows.shape[0]):
+            for r in range(windows.shape[1]):
+                node_id = q*19 + r
+                patch = windows[q,r].reshape(-1).astype(np.uint32)
+                for k in patch.nonzero()[0]:
+                    graphs_test.add_node_feature(i, node_id, k)
+                graphs_test.add_node_feature(i, node_id, 'c:'+str(q))
+                graphs_test.add_node_feature(i, node_id, 'r:'+str(r))
+
+graphs_test.encode()
+
+print("Testing data produced")
+
+tm = MultiClassGraphTsetlinMachine(args.number_of_clauses, args.T, args.s, (1, 19*19, args.hypervector_size), (1, 1), max_included_literals=args.max_included_literals)
+
+for i in range(args.epochs):
+    start_training = time()
+    tm.fit(graphs_train, Y_train, epochs=1, incremental=True)
+    stop_training = time()
+
+    start_testing = time()
+    result_test = 100*(tm.predict(graphs_test) == Y_test).mean()
+    stop_testing = time()
+
+    result_train = 100*(tm.predict(graphs_train) == Y_train).mean()
+
+    print("%d %.2f %.2f %.2f %.2f" % (i, result_train, result_test, stop_training-start_training, stop_testing-start_testing))
+
+weights = tm.get_state()[1].reshape(2, -1)
+for i in range(tm.number_of_clauses):
+        print("Clause #%d W:(%d %d)" % (i, weights[0,i], weights[1,i]), end=' ')
+        l = []
+        for k in range(args.hypervector_size * 2):
+            if tm.ta_action(i, k):
+                if k < args.hypervector_size:
+                    l.append("x%d" % (k))
+                else:
+                    l.append("NOT x%d" % (k - args.hypervector_size))
+        print(" AND ".join(l))
+
+
+start_training = time()
+tm.fit(graphs_train, Y_train, epochs=1, incremental=True)
+stop_training = time()
+
+start_testing = time()
+result_test = 100*(tm.predict(graphs_test) == Y_test).mean()
+stop_testing = time()
+
+result_train = 100*(tm.predict(graphs_train) == Y_train).mean()
+
+print("%.2f %.2f %.2f %.2f" % (result_train, result_test, stop_training-start_training, stop_testing-start_testing))
+
+print(graphs_train.hypervectors)
--- examples/SimpleGraphDemoTest.py
+++ examples/SimpleGraphDemoTest.py
@@ -1,72 +1,106 @@
-from GraphTsetlinMachine.graph import Graph
-from GraphTsetlinMachine.graph import Graphs
+from PySparseCoalescedTsetlinMachineCUDA.graphs import Graphs
 import numpy as np
 from scipy.sparse import csr_matrix
-from GraphTsetlinMachine.tm import MultiClassGraphTsetlinMachine
+from PySparseCoalescedTsetlinMachineCUDA.tm import MultiClassGraphTsetlinMachine
 from time import time
+import argparse
 
-epochs = 100
+def default_args(**kwargs):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--epochs", default=25, type=int)
+    parser.add_argument("--number-of-clauses", default=2, type=int)
+    parser.add_argument("--T", default=16, type=int)
+    parser.add_argument("--s", default=1.0, type=float)
+    parser.add_argument("--hypervector_size", default=16, type=int)
+    parser.add_argument("--hypervector_bits", default=1, type=int)
+    parser.add_argument("--noise", default=0.0, type=float)
+    parser.add_argument("--number-of-examples", default=1000, type=int)
+    parser.add_argument("--max-sequence-length", default=10, type=int)
+    parser.add_argument("--number-of-classes", default=2, type=int)
+    parser.add_argument("--max-included-literals", default=1, type=int)
 
-number_of_training_examples = 1000
+    args = parser.parse_args()
+    for key, value in kwargs.items():
+        if key in args.__dict__:
+            setattr(args, key, value)
+    return args
 
-max_sequence_length = 1
+args = default_args()
 
-number_of_classes = 2 # Must be less than or equal to max sequence length
+number_of_nodes_train = np.empty(args.number_of_examples, dtype=np.uint32)
+for i in range(args.number_of_examples):
+    number_of_nodes_train[i] = np.random.randint(1, args.max_sequence_length)
 
-graphs_train = Graphs()
-Y_train = np.empty(number_of_training_examples, dtype=np.uint32)
+graphs_train = Graphs(number_of_nodes_train, symbols=['A', 'B'], hypervector_size=args.hypervector_size, hypervector_bits=args.hypervector_bits)
+Y_train = np.empty(args.number_of_examples, dtype=np.uint32)
 
-for i in range(number_of_training_examples):
-    sequence_graph = Graph()
-    
+for i in range(args.number_of_examples):
     # Select class
-    Y_train[i] = np.random.randint(number_of_classes) 
+    Y_train[i] = np.random.randint(args.number_of_classes)
 
-    nodes = 1000
-    for j in range(nodes):
-        sequence_graph.add_node(j)
+    feature_pos = np.random.randint(number_of_nodes_train[i])
+    for j in range(number_of_nodes_train[i]):
+        if feature_pos == j:
+            if Y_train[i] == 0:
+                graphs_train.add_node_feature(i, j, 'A')
+            else:
+                graphs_train.add_node_feature(i, j, 'B')
+   
+Y_train = np.where(np.random.rand(args.number_of_examples) < args.noise, 1 - Y_train, Y_train)  # Adds noise
 
-    j = np.random.randint(nodes)
+graphs_train.encode()
 
-    if Y_train[i] == 0:
-        sequence_graph.add_feature(j, 'A')
-    else:
-        sequence_graph.add_feature(j, 'B')
+number_of_nodes_test = np.empty(args.number_of_examples, dtype=np.uint32)
+for i in range(args.number_of_examples):
+    number_of_nodes_test[i] = np.random.randint(1, args.max_sequence_length)
 
-    graphs_train.add(sequence_graph)
+graphs_test = Graphs(number_of_nodes_test, init_with=graphs_train)
+Y_test = np.empty(args.number_of_examples, dtype=np.uint32)
 
-Y_train = np.where(np.random.rand(number_of_training_examples) < 0, 1 - Y_train, Y_train)  # Adds noise
+for i in range(args.number_of_examples):
+    # Select class
+    Y_test[i] = np.random.randint(args.number_of_classes)
 
-tm = MultiClassGraphTsetlinMachine(10, 80, 5.0, hypervector_size=256, hypervector_bits=3, depth=1)
+    feature_pos = np.random.randint(number_of_nodes_test[i])
+    for j in range(number_of_nodes_test[i]):
+        if feature_pos == j:
+            if Y_test[i] == 0:
+                graphs_test.add_node_feature(i, j, 'A')
+            else:
+                graphs_test.add_node_feature(i, j, 'B')
+   
+graphs_test.encode()
 
-for i in range(epochs):
+print(graphs_test.hypervectors)
+
+graphs_train.Y = Y_train
+graphs_test.Y = Y_test
+
+tm = MultiClassGraphTsetlinMachine(args.number_of_clauses, args.T, args.s, (1, args.max_sequence_length, args.hypervector_size), (1, 1), max_included_literals=args.max_included_literals)
+
+for i in range(args.epochs):
     start_training = time()
     tm.fit(graphs_train, Y_train, epochs=1, incremental=True)
     stop_training = time()
 
     start_testing = time()
-    result_test = 100*(tm.predict(graphs_train) == Y_train).mean()
+    result_test = 100*(tm.predict(graphs_test) == Y_test).mean()
     stop_testing = time()
 
     result_train = 100*(tm.predict(graphs_train) == Y_train).mean()
 
     print("%d %.2f %.2f %.2f %.2f" % (i, result_train, result_test, stop_training-start_training, stop_testing-start_testing))
 
-print(graphs_train.X)
-print()
-print(graphs_train.edges)
-print(graphs_train.hypervectors)
-print(graphs_train.edge_type_id)
-print(graphs_train.node_count)
-
 weights = tm.get_state()[1].reshape(2, -1)
 for i in range(tm.number_of_clauses):
         print("Clause #%d W:(%d %d)" % (i, weights[0,i], weights[1,i]), end=' ')
         l = []
-        for k in range(16 * 2):
+        for k in range(args.hypervector_size * 2):
             if tm.ta_action(i, k):
-                if k < 16:
-                    l.append(" x%d" % (k))
+                if k < args.hypervector_size:
+                    l.append("x%d" % (k))
                 else:
-                    l.append(" NOT x%d" % (k - 16))
+                    l.append("NOT x%d" % (k - args.hypervector_size))
         print(" AND ".join(l))
+
+print(graphs_train.hypervectors)
