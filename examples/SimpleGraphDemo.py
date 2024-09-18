c40cb40474aedd2151dd712db3cf88d27bbe5ed5
Ole-Christoffer Granmo <ole.granmo@uia.no>
2024-09-18 16:13:47 +0200

    Update

--- examples/SimpleGraphDemo.py
+++ examples/SimpleGraphDemo.py
@@ -69,7 +69,7 @@ graphs_test.encode()
 
 print(graphs_test.hypervectors)
 
-tm = MultiClassGraphTsetlinMachine(args.number_of_clauses, args.T, args.s, (1, 1), max_included_literals=args.max_included_literals)
+tm = MultiClassGraphTsetlinMachine(args.number_of_clauses, args.T, args.s, max_included_literals=args.max_included_literals)
 
 for i in range(args.epochs):
     start_training = time()
