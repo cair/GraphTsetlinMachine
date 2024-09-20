179c494fdba8362dd0634d366b33d0c55d78dde7
Ole-Christoffer Granmo <ole.granmo@uia.no>
2024-09-20 18:12:30 +0200

    Update

--- examples/SimpleGraphDemo.py
+++ examples/SimpleGraphDemo.py
@@ -1,7 +1,7 @@
 from GraphTsetlinMachine.graphs import Graphs
 import numpy as np
 from scipy.sparse import csr_matrix
-#from GraphTsetlinMachine.tm import MultiClassGraphTsetlinMachine
+from GraphTsetlinMachine.tm import MultiClassGraphTsetlinMachine
 from time import time
 import argparse
 
