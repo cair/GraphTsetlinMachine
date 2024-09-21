014733e431d9295722bbb8e1c170f740e6257af4
Ole-Christoffer Granmo <ole.granmo@uia.no>
2024-09-21 12:33:03 +0200

    Update

--- examples/SimpleGraphDemo.py
+++ examples/SimpleGraphDemo.py
@@ -58,6 +58,7 @@ for graph_id in range(args.number_of_examples):
             graphs_train.add_graph_node_edge(graph_id, node_id, destination_node_id, edge_type)
 
     Y_train[graph_id] = np.random.randint(args.number_of_classes)
+    node_id = 0
     if Y_train[graph_id] == 0:
         graphs_train.add_graph_node_feature(graph_id, node_id, 'A')
     else:
@@ -98,6 +99,7 @@ for graph_id in range(args.number_of_examples):
             graphs_test.add_graph_node_edge(graph_id, node_id, destination_node_id, edge_type)
 
     Y_test[graph_id] = np.random.randint(args.number_of_classes)
+    node_id = 0
     if Y_test[graph_id] == 0:
         graphs_test.add_graph_node_feature(graph_id, node_id, 'A')
     else:
