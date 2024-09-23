316a51e5ef3fac174291ab586721dcfdf7f5f285
Ole-Christoffer Granmo <ole.granmo@uia.no>
2024-09-23 13:49:45 +0200

    Update

--- GraphTsetlinMachine/kernels.py
+++ GraphTsetlinMachine/kernels.py
@@ -425,11 +425,11 @@ code_evaluate = """
                         clause_output = 0;
                     }
 
-                    //int clause_chunk = clause / INT_SIZE;
-                    //int clause_pos = clause % INT_SIZE;
-                    //global_clause_output[clause_chunk*MAX_NODES*128 + patch*128 + threadIdx.x] = (1 << clause_pos);
+                    int clause_chunk = clause / INT_SIZE;
+                    int clause_pos = clause % INT_SIZE;
+                    global_clause_output[clause_chunk*MAX_NODES*128 + patch*128 + threadIdx.x] = (1 << clause_pos);
 
-                    global_clause_output[clause*MAX_NODES + patch] = clause_output;
+                    //global_clause_output[clause*MAX_NODES + patch] = clause_output;
                 }
             }
         }
