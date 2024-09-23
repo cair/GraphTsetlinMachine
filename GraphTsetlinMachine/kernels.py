4c152ec9bfd84d18977d7e02aa8eeca3be5ad1b9
Ole-Christoffer Granmo <ole.granmo@uia.no>
2024-09-23 12:31:17 +0200

    Update

--- GraphTsetlinMachine/kernels.py
+++ GraphTsetlinMachine/kernels.py
@@ -394,10 +394,11 @@ code_evaluate = """
 
             unsigned int *X = &global_X[graph_index * LA_CHUNKS];
 
-            int local_clause_output[INT_SIZE * MAX_NODES];
 
             for (int clause_int = index; clause_int < CLAUSES/INT_SIZE; clause_int += stride) {
                 for (int patch = 0; patch < number_of_nodes; ++patch) {
+                    unsigned int local_clause_output;
+
                     for (int k = 0; k < INT_SIZE; ++k) {
                         int clause = clause_int*INT_SIZE + k;
                         unsigned int *ta_state = &global_ta_state[clause*LA_CHUNKS*STATE_BITS];
@@ -414,14 +415,14 @@ code_evaluate = """
                         }
 
                         if (clause_output) {
-                            local_clause_output[patch] |= (1 << k);
+                            local_clause_output |= (1 << k);
                         } else {
-                            local_clause_output[patch] &= ~(1 << k);
+                            local_clause_output &= ~(1 << k);
                         }
                     }
-                }
 
-                global_clause_output[clause_int*MAX_NODES + patch] = local_clause_output[patch];
+                    global_clause_output[clause_int*MAX_NODES + patch] = local_clause_output;
+                }
             }
         }
 
