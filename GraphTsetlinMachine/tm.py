65b12ff023f0e1f1d861caa4346bdb9334a66842
Ole-Christoffer Granmo <ole.granmo@uia.no>
2024-09-26 12:21:44 +0200

    Update

--- GraphTsetlinMachine/tm.py
+++ GraphTsetlinMachine/tm.py
@@ -65,7 +65,7 @@ class CommonTsetlinMachine():
 		self.boost_true_positive_feedback = boost_true_positive_feedback
 		self.depth = depth
 		self.message_size = message_size
-		self.message_bits = message_bits
+		self.message_bits = 2#message_bits
 		self.message_prime = prevprime(message_size//2)
 		self.hypervector_literals = message_size*2
 		self.hypervector_chunks = (self.hypervector_literals - 1) // 32 + 1
