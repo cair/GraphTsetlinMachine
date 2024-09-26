e7bb11b2ed85b0e6bce50643598a7ada508df72a
Ole-Christoffer Granmo <ole.granmo@uia.no>
2024-09-26 18:00:28 +0200

    Updaste

--- hashing_test.py
+++ hashing_test.py
@@ -1,6 +1,14 @@
 import argparse
 import numpy as np
 from sympy import prevprime
+import mmh3
+
+def murmur(key, seed):
+    for i in range(4):
+        seed ^= key[i]
+        seed *= 0x5bd1e995
+        seed ^= seed >> 15
+    return seed
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--items", default=3000, type=int)
@@ -8,82 +16,6 @@ parser.add_argument("--inserts", default=100, type=int)
 parser.add_argument("--bits", default=16384, type=int)
 
 args = parser.parse_args()
-
-prime = prevprime(args.bits // 2)
-
-collision = {}
-collisions = 0
-for i in range(args.items):
-    first_bit = i % (args.bits // 2)
-    second_bit = prime - (i % prime)
-
-    if (first_bit, second_bit) in collision:
-        collisions += 1
-    collision[(first_bit, second_bit)] = 1
-#    collision[(second_bit, first_bit)] = 1
-
-collision_local = {}
-collisions_insert = 0
-for j in range(args.inserts):
-    i = np.random.randint(args.items)
-    first_bit = i % (args.bits // 2)
-    second_bit = prime - (i % prime)
-
-    if (first_bit in collision_local) and (second_bit in collision_local):
-        collisions_insert += 1
-
-    collision_local[first_bit] = 1
-    collision_local[second_bit] = 1
-
-print(collisions_insert, collisions)
-
-collision = {}
-collisions = 0
-for i in range(args.items):
-    sqrt_i = int(np.sqrt(i))
-    sqrt_i_2 = sqrt_i**2
-
-    if i - sqrt_i_2 < sqrt_i:
-        first_bit = i - sqrt_i_2
-        second_bit = sqrt_i
-    else:
-        second_bit = i - sqrt_i_2 - sqrt_i
-        first_bit = sqrt_i
-
-    first_bit = first_bit % args.bits
-    second_bit = second_bit % args.bits
-
-    if (first_bit, second_bit) in collision:
-        collisions += 1
-    collision[(first_bit, second_bit)] = 1
-    collision[(second_bit, first_bit)] = 1
-
-collision_first = {}
-collision_second = {}
-collisions_insert = 0
-for j in range(args.inserts):
-    i = np.random.randint(args.items)
- 
-    sqrt_i = int(np.sqrt(i))
-    sqrt_i_2 = sqrt_i**2
-
-    if i - sqrt_i_2 < sqrt_i:
-        first_bit = i - sqrt_i_2
-        second_bit = sqrt_i
-    else:
-        second_bit = i - sqrt_i_2 - sqrt_i
-        first_bit = sqrt_i
-
-    first_bit = first_bit % args.bits
-    second_bit = second_bit % args.bits
-
-    if (first_bit in collision_local) and (second_bit in collision_local):
-        collisions_insert += 1
-
-    collision_local[first_bit] = 1
-    collision_local[second_bit] = 1
-
-print(collisions_insert, collisions)
 
 hypervectors = {}
 collisions = 0
@@ -99,22 +31,25 @@ for i in range(args.items):
     collision[(first_bit, second_bit)] = 1
     collision[(second_bit, first_bit)] = 1
 
-collision_local = {}
-collisions_insert = 0
-
 hypervectors = list(hypervectors.keys())
-for j in range(args.inserts):
-    i = np.random.randint(len(hypervectors))
+collisions_insert_average = 0
+for k in range(1000):
+    collision_local = {}
+    collisions_insert = 0
 
-    (first_bit, second_bit) = hypervectors[i]
+    for j in range(args.inserts):
+        i = np.random.randint(len(hypervectors))
 
-    if (first_bit in collision_local) and (second_bit in collision_local):
-        collisions_insert += 1
+        (first_bit, second_bit) = hypervectors[i]
 
-    collision_local[first_bit] = 1
-    collision_local[second_bit] = 1
+        if (first_bit in collision_local) and (second_bit in collision_local):
+            collisions_insert += 1
 
-print(collisions_insert, collisions)
+        collision_local[first_bit] = 1
+        collision_local[second_bit] = 1
+    collisions_insert_average += collisions_insert/1000.0
+
+print(collisions_insert_average, collisions)
 
 hypervectors = {}
 collision = {}
@@ -135,20 +70,65 @@ for i in range(args.items):
     collision[(second_bit, third_bit, first_bit)] = 1
     collision[(third_bit, first_bit, second_bit)] = 1
 
-collision_local = {}
-collisions_insert = 0
-
 hypervectors = list(hypervectors.keys())
-for j in range(args.inserts):
-    i = np.random.randint(len(hypervectors))
+collisions_insert_average = 0
+for k in range(1000):
+    collision_local = {}
+    collisions_insert = 0
 
-    (first_bit, second_bit, third_bit) = hypervectors[i]
+    for j in range(args.inserts):
+        i = np.random.randint(len(hypervectors))
 
-    if (first_bit in collision_local) and (second_bit in collision_local) and (third_bit in collision_local):
-        collisions_insert += 1
+        (first_bit, second_bit, third_bit) = hypervectors[i]
 
-    collision_local[first_bit] = 1
-    collision_local[second_bit] = 1
-    collision_local[third_bit] = 1
+        if (first_bit in collision_local) and (second_bit in collision_local) and (third_bit in collision_local):
+            collisions_insert += 1
 
-print(collisions_insert, collisions)
+        collision_local[first_bit] = 1
+        collision_local[second_bit] = 1
+        collision_local[third_bit] = 1
+    
+    collisions_insert_average += collisions_insert/1000.0
+
+print(collisions_insert_average, collisions)
+
+collision = {}
+collisions = 0
+for i in range(args.items):
+    i = np.uint32(i).tobytes()
+    first_bit = murmur(i, 0x81726354) % (args.bits)
+    second_bit = murmur(i, 0x12345678) % (args.bits)
+    third_bit = murmur(i, 0x87654321) % (args.bits)
+
+    if (first_bit, second_bit, third_bit) in collision:
+        collisions += 1
+
+    collision[(first_bit, second_bit, third_bit)] = 1
+    collision[(second_bit, first_bit, third_bit)] = 1
+    collision[(third_bit, second_bit, first_bit)] = 1
+    collision[(first_bit, third_bit, second_bit)] = 1
+    collision[(second_bit, third_bit, first_bit)] = 1
+    collision[(third_bit, first_bit, second_bit)] = 1
+
+collisions_insert_average = 0
+for k in range(1000):
+    collision_local = {}
+    collisions_insert = 0
+
+    for j in range(args.inserts):
+        i = np.random.randint(args.items, dtype=np.uint32).tobytes()
+
+        first_bit = murmur(i, 0x81726354) % (args.bits)
+        second_bit = murmur(i, 0x12345678) % (args.bits)
+        third_bit = murmur(i, 0x87654321) % (args.bits)
+
+        if (first_bit in collision_local) and (second_bit in collision_local) and (third_bit in collision_local):
+            collisions_insert += 1
+
+        collision_local[first_bit] = 1
+        collision_local[second_bit] = 1
+        collision_local[third_bit] = 1
+    
+    collisions_insert_average += collisions_insert/1000.0
+
+print(collisions_insert_average, collisions)
