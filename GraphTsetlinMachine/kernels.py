7d3058b5690a6fd919c5754a299fbb17acaeb09f
Ole-Christoffer Granmo <ole.granmo@uia.no>
2024-09-26 20:00:40 +0200

    Update

--- GraphTsetlinMachine/kernels.py
+++ GraphTsetlinMachine/kernels.py
@@ -479,11 +479,11 @@ code_evaluate = """
                 //     bit[bit_index] = hypervectors[clause*MESSAGE_BITS + bit_index];
                 //}
 
-                //bit[0] = clause % (MESSAGE_SIZE / 2);
-                //bit[1] = (MESSAGE_SIZE / 2) + MESSAGE_PRIME - (clause % MESSAGE_PRIME);
+                bit[0] = clause % (MESSAGE_SIZE / 2);
+                bit[1] = (MESSAGE_SIZE / 2) + MESSAGE_PRIME - (clause % MESSAGE_PRIME);
 
-                bit[0] = jenkins(clause, 0x81726354) % MESSAGE_SIZE;
-                bit[1] = jenkins(clause, 0x12345678) % MESSAGE_SIZE;
+                //bit[0] = jenkins(clause, 0x81726354) % MESSAGE_SIZE;
+                //bit[1] = jenkins(clause, 0x12345678) % MESSAGE_SIZE;
 
                 //bit[0] = murmur(clause, 0x81726354) % MESSAGE_SIZE;
                 //bit[1] = murmur(clause, 0x12345678) % MESSAGE_SIZE;
