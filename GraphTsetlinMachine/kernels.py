a11210f25513c8a8c7dd96db0b919c78c8d0930c
Ole-Christoffer Granmo <ole.granmo@uia.no>
2024-09-26 18:20:13 +0200

    Update

--- GraphTsetlinMachine/kernels.py
+++ GraphTsetlinMachine/kernels.py
@@ -437,9 +437,10 @@ code_evaluate = """
         }
 
         __device__ inline unsigned int murmur(unsigned int key, unsigned int h)
-        {        
+        {
+            unsigned char *key_char = &key;
             for (int i = 0; i < 4; ++i) {
-                h ^= (key >> (8*i)) & 0xff;
+                h ^= key[i];
                 h *= 0x5bd1e995;
                 h ^= h >> 15;
             }
@@ -464,11 +465,11 @@ code_evaluate = """
                 //     bit[bit_index] = hypervectors[clause*MESSAGE_BITS + bit_index];
                 //}
 
-                bit[0] = clause % (MESSAGE_SIZE / 2);
-                bit[1] = (MESSAGE_SIZE / 2) + MESSAGE_PRIME - (clause % MESSAGE_PRIME);
+                //bit[0] = clause % (MESSAGE_SIZE / 2);
+                //bit[1] = (MESSAGE_SIZE / 2) + MESSAGE_PRIME - (clause % MESSAGE_PRIME);
 
-                //bit[0] = murmur(clause, 0x81726354) % MESSAGE_SIZE;
-                //bit[1] = murmur(clause, 0x12345678) % MESSAGE_SIZE;
+                bit[0] = murmur(clause, 0x81726354) % MESSAGE_SIZE;
+                bit[1] = murmur(clause, 0x12345678) % MESSAGE_SIZE;
 
                 for (int node = 0; node < number_of_nodes; ++node) {
                     int node_chunk = node / INT_SIZE;
