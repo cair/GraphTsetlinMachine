ab9d5e6ae6fec68fd148fa8b095fa41b7ee0aad0
Ole-Christoffer Granmo <ole.granmo@uia.no>
2024-09-30 20:05:10 +0200

    Update

--- GraphTsetlinMachine/tm.py
+++ GraphTsetlinMachine/tm.py
@@ -498,6 +498,11 @@ class CommonTsetlinMachine():
 		cuda.memcpy_dtoh(clause_X_int_test, self.clause_X_int_test_gpu)
 		print(clause_X_int_test)
 
+
+		clause_X_test = np.empty((graphs.max_number_of_graph_nodes, self.number_of_message_chunks), dtype=np.uint32)
+		cuda.memcpy_dtoh(clause_X_test, self.clause_X_test_gpu[0])
+		print(clause_X_test)
+
 		cuda.memcpy_dtoh(class_sum[e,:], self.class_sum_gpu)
 
 		return class_sum
