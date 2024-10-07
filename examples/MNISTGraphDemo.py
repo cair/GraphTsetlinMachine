58054260020f8b4650e4aa4ed03a97f981798fd9
Ole-Christoffer Granmo <ole.granmo@uia.no>
2024-10-07 12:12:24 +0200

    Update

--- examples/MNISTGraphDemo.py
+++ examples/MNISTGraphDemo.py
@@ -22,7 +22,7 @@ def default_args(**kwargs):
     parser.add_argument("--T", default=1000, type=int)
     parser.add_argument("--s", default=1.0, type=float)
     parser.add_argument("--depth", default=1, type=int)
-    parser.add_argument("--hypervector-size", default=64, type=int)
+    parser.add_argument("--hypervector-size", default=128, type=int)
     parser.add_argument("--hypervector-bits", default=2, type=int)
     parser.add_argument("--message-size", default=256, type=int)
     parser.add_argument("--message-bits", default=2, type=int)
