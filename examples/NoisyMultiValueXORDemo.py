1f625af400d89cb3631d3779d8da4378cbd6bbc4
Ole-Christoffer Granmo <ole.granmo@uia.no>
2024-10-12 11:52:49 +0200

    Update

--- examples/NoisyMultiValueXORDemo.py
+++ examples/NoisyMultiValueXORDemo.py
@@ -12,6 +12,7 @@ def default_args(**kwargs):
     parser.add_argument("--number-of-clauses", default=10, type=int)
     parser.add_argument("--T", default=100, type=int)
     parser.add_argument("--s", default=1.0, type=float)
+    parser.add_argument("--q", default=0.5, type=float)
     parser.add_argument("--depth", default=2, type=int)
     parser.add_argument("--hypervector-size", default=256, type=int)
     parser.add_argument("--hypervector-bits", default=2, type=int)
@@ -22,6 +23,7 @@ def default_args(**kwargs):
     parser.add_argument("--number-of-values", default=2, type=int)
     parser.add_argument("--number-of-examples", default=10000, type=int)
     parser.add_argument("--max-included-literals", default=4, type=int)
+    parser.add_argument("--number-of-state-bits", default=10, type=int)
 
     args = parser.parse_args()
     for key, value in kwargs.items():
