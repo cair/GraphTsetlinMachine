7fbdc84791b5dbe088afe2c59a03359ae88e0438
Ole-Christoffer Granmo <ole.granmo@uia.no>
2024-10-12 14:26:56 +0200

    Update

--- examples/MNISTGraphDemo.py
+++ examples/MNISTGraphDemo.py
@@ -21,7 +21,7 @@ def default_args(**kwargs):
     parser.add_argument("--number-of-clauses", default=1000, type=int)
     parser.add_argument("--T", default=10000, type=int)
     parser.add_argument("--s", default=1.0, type=float)
-    parser.add_argument("--q", default=0.5, type=float)
+    parser.add_argument("--q", default=1.0, type=float)
     parser.add_argument("--depth", default=1, type=int)
     parser.add_argument("--hypervector-size", default=128, type=int)
     parser.add_argument("--hypervector-bits", default=2, type=int)
@@ -29,7 +29,7 @@ def default_args(**kwargs):
     parser.add_argument("--message-bits", default=2, type=int)
     parser.add_argument('--double-hashing', dest='double_hashing', default=False, action='store_true')
     parser.add_argument("--max-included-literals", default=32, type=int)
-    parser.add_argument("--number-of-state-bits", default=10, type=int)
+    parser.add_argument("--number-of-state-bits", default=12, type=int)
 
     args = parser.parse_args()
     for key, value in kwargs.items():
