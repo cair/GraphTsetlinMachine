1175517ce28d5931634d06bdbb53f87a7a1f95c9
Ole-Christoffer Granmo <ole.granmo@uia.no>
2024-10-17 20:43:23 +0200

    Update

--- examples/NoisyXORDemo.py
+++ examples/NoisyXORDemo.py
@@ -12,6 +12,7 @@ def default_args(**kwargs):
     parser.add_argument("--number-of-clauses", default=10, type=int)
     parser.add_argument("--T", default=100, type=int)
     parser.add_argument("--s", default=1.0, type=float)
+    parser.add_argument("--number-of-state-bits", default=8, type=int)
     parser.add_argument("--depth", default=2, type=int)
     parser.add_argument("--hypervector-size", default=32, type=int)
     parser.add_argument("--hypervector-bits", default=2, type=int)
@@ -118,6 +119,7 @@ tm = MultiClassGraphTsetlinMachine(
     args.number_of_clauses,
     args.T,
     args.s,
+    number_of_state_bits = args.number_of_state_bits,
     depth = args.depth,
     message_size = args.message_size,
     message_bits = args.message_bits,
