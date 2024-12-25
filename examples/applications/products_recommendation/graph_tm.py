c4546310371c7dbcb0ccad959223c087c0a6669c
Ahmed Khalid <a.k.kadhim@outlook.com>
2024-12-25 13:04:24 +0000

    add main.bash

--- examples/applications/products_recommendation/graph_tm.py
+++ examples/applications/products_recommendation/graph_tm.py
@@ -1,9 +1,145 @@
 from GraphTsetlinMachine.graphs import Graphs
 from GraphTsetlinMachine.tm import MultiClassGraphTsetlinMachine
-from time import time
 import argparse
 import numpy as np
 import prepare_dataset
+import pandas as pd
+from tmu.tools import BenchmarkTimer
+import os
+
+def main(args):  
+    np.random.seed(42)
+    results = []
+    data = prepare_dataset.aug_amazon_products(noise_ratio = args.dataset_noise_ratio)
+    x, y = prepare_dataset.construct_x_y(data)
+    X_train, X_test, Y_train, Y_test = prepare_dataset.train_test_split(x,y)
+    users = data['user_id'].unique()
+    items = data['product_id'].unique()
+    categories = data['category'].unique()
+    # Initialize Graphs with symbols for GTM
+    number_of_nodes = 3
+    symbols = []
+    symbols = ["U_" + str(u) for u in users] + ["I_" + str(i) for i in items] + ["C_" + str(c) for c in categories] 
+    print("Symbols: ",len(symbols))
+
+    # Train data
+    graphs_train = Graphs(
+        X_train.shape[0],
+        symbols=symbols,
+        hypervector_size=args.hypervector_size,
+        hypervector_bits=args.hypervector_bits,
+        double_hashing = args.double_hashing
+    )
+    for graph_id in range(X_train.shape[0]):
+        graphs_train.set_number_of_graph_nodes(graph_id, number_of_nodes)
+    graphs_train.prepare_node_configuration()
+    for graph_id in range(X_train.shape[0]):
+        for node_id in range(graphs_train.number_of_graph_nodes[graph_id]):
+            number_of_edges = 2 if node_id > 0 and node_id < graphs_train.number_of_graph_nodes[graph_id]-1 else 1
+            if node_id == 0:
+                graphs_train.add_graph_node(graph_id, "User", number_of_edges)
+            elif node_id == 1:
+                graphs_train.add_graph_node(graph_id, "Item", number_of_edges)
+            else:
+                graphs_train.add_graph_node(graph_id, "Category", number_of_edges)
+    graphs_train.prepare_edge_configuration()
+    for graph_id in range(X_train.shape[0]):
+        for node_id in range(graphs_train.number_of_graph_nodes[graph_id]):
+            if node_id == 0:
+                graphs_train.add_graph_node_edge(graph_id, "User", "Item", "UserItem")
+                
+            if node_id == 1:
+                graphs_train.add_graph_node_edge(graph_id, "Item", "Category", "ItemCategory")
+                graphs_train.add_graph_node_edge(graph_id, "Item", "User", "ItemUser")
+                
+            if node_id == 2:
+                graphs_train.add_graph_node_edge(graph_id, "Category", "Item", "CatrgoryItem")
+
+        graphs_train.add_graph_node_property(graph_id, "User", "U_" + str(X_train[graph_id][0]))
+        graphs_train.add_graph_node_property(graph_id, "Item", "I_" + str(X_train[graph_id][1]))
+        graphs_train.add_graph_node_property(graph_id, "Category", "C_" + str(X_train[graph_id][2]))
+    graphs_train.encode()
+    print("Training data produced")
+
+    # Test data
+    graphs_test = Graphs(X_test.shape[0], init_with=graphs_train)
+    for graph_id in range(X_test.shape[0]):
+        graphs_test.set_number_of_graph_nodes(graph_id, number_of_nodes)
+    graphs_test.prepare_node_configuration()
+    for graph_id in range(X_test.shape[0]):
+        for node_id in range(graphs_test.number_of_graph_nodes[graph_id]):
+            number_of_edges = 2 if node_id > 0 and node_id < graphs_test.number_of_graph_nodes[graph_id]-1 else 1
+            if node_id == 0:
+                graphs_test.add_graph_node(graph_id, "User", number_of_edges)
+            elif node_id == 1:
+                graphs_test.add_graph_node(graph_id, "Item", number_of_edges)
+            else:
+                graphs_test.add_graph_node(graph_id, "Category", number_of_edges)
+    graphs_test.prepare_edge_configuration()
+    for graph_id in range(X_test.shape[0]):
+        for node_id in range(graphs_test.number_of_graph_nodes[graph_id]):
+            if node_id == 0:
+                graphs_test.add_graph_node_edge(graph_id, "User", "Item", "UserItem")
+                
+            if node_id == 1:
+                graphs_test.add_graph_node_edge(graph_id, "Item", "Category", "ItemCategory")
+                graphs_test.add_graph_node_edge(graph_id, "Item", "User", "ItemUser")
+                
+            if node_id == 2:
+                graphs_test.add_graph_node_edge(graph_id, "Category", "Item", "CatrgoryItem")
+
+        graphs_test.add_graph_node_property(graph_id, "User", "U_" + str(X_test[graph_id][0]))
+        graphs_test.add_graph_node_property(graph_id, "Item", "I_" + str(X_test[graph_id][1]))
+        graphs_test.add_graph_node_property(graph_id, "Category", "C_" + str(X_test[graph_id][2]))
+    graphs_test.encode()
+    print("Testing data produced")
+
+    tm = MultiClassGraphTsetlinMachine(
+        args.number_of_clauses,
+        args.T,
+        args.s,
+        number_of_state_bits = args.number_of_state_bits,
+        depth=args.depth,
+        message_size=args.message_size,
+        message_bits=args.message_bits,
+        max_included_literals=args.max_included_literals,
+        double_hashing = args.double_hashing
+    )
+
+    for epoch in range(args.epochs):
+        benchmark_total = BenchmarkTimer(logger=None, text="Epoch Time")
+        with benchmark_total:
+            benchmark1 = BenchmarkTimer(logger=None, text="Training Time")
+            with benchmark1:
+                tm.fit(graphs_train, Y_train, epochs=1, incremental=True)
+            train_time = benchmark1.elapsed()
+            
+            benchmark2 = BenchmarkTimer(logger=None, text="Testing Time")
+            with benchmark2:
+                accuracy = 100*(tm.predict(graphs_test) == Y_test).mean()
+            test_time = benchmark2.elapsed()
+        total_time = benchmark_total.elapsed()
+        # result_train = 100*(tm.predict(graphs_train) == Y_train).mean()
+    results.append({
+        "Algorithm": "GraphTM",
+        "Noise_Ratio": args.dataset_noise_ratio,
+        "T": args.T,
+        "s": args.s,
+        "Max_Included_Literals": args.max_included_literals,
+        "Epochs": args.epochs,
+        "Platform": "CUDA",
+        "Total_Time": total_time,
+        "Accuracy": accuracy,
+    })
+
+    # Save results to CSV
+    results_df = pd.DataFrame(results)
+    results_file = "experiment_results.csv"
+    if os.path.exists(results_file):
+        results_df.to_csv(results_file, mode='a', index=False, header=False)
+    else:
+        results_df.to_csv(results_file, index=False)
+    print(f"Results saved to {results_file}")
 
 def default_args(**kwargs):
     parser = argparse.ArgumentParser()
@@ -20,145 +156,12 @@ def default_args(**kwargs):
     parser.add_argument('--double-hashing', dest='double_hashing', default=False, action='store_true')
     parser.add_argument("--noise", default=0.01, type=float)
     parser.add_argument("--max-included-literals", default=23, type=int)
-
+    parser.add_argument("--dataset_noise_ratio", default=0.01, type=float)
     args = parser.parse_args()
     for key, value in kwargs.items():
         if key in args.__dict__:
             setattr(args, key, value)
     return args
-args = default_args()
-np.random.seed(42)
 
-# data = prepare_dataset.amazon_products()
-data = prepare_dataset.aug_amazon_products()
-# data = prepare_dataset.artificial()
-# data = prepare_dataset.artificial_with_user_pref()
-# data = prepare_dataset.artificial_pattered()
-# print(data.head())
-x, y = prepare_dataset.construct_x_y(data)
-X_train, X_test, Y_train, Y_test = prepare_dataset.train_test_split(x,y)
-users = data['user_id'].unique()
-items = data['product_id'].unique()
-categories = data['category'].unique()
-# Initialize Graphs with symbols for GTM
-number_of_nodes = 3
-symbols = []
-symbols = ["U_" + str(u) for u in users] + ["I_" + str(i) for i in items] + ["C_" + str(c) for c in categories] 
-print("Symbols: ",len(symbols))
-
-# Train data
-graphs_train = Graphs(
-    X_train.shape[0],
-    symbols=symbols,
-    hypervector_size=args.hypervector_size,
-    hypervector_bits=args.hypervector_bits,
-    double_hashing = args.double_hashing
-)
-for graph_id in range(X_train.shape[0]):
-    graphs_train.set_number_of_graph_nodes(graph_id, number_of_nodes)
-graphs_train.prepare_node_configuration()
-for graph_id in range(X_train.shape[0]):
-    for node_id in range(graphs_train.number_of_graph_nodes[graph_id]):
-        number_of_edges = 2 if node_id > 0 and node_id < graphs_train.number_of_graph_nodes[graph_id]-1 else 1
-        if node_id == 0:
-            graphs_train.add_graph_node(graph_id, "User", number_of_edges)
-        elif node_id == 1:
-            graphs_train.add_graph_node(graph_id, "Item", number_of_edges)
-        else:
-            graphs_train.add_graph_node(graph_id, "Category", number_of_edges)
-graphs_train.prepare_edge_configuration()
-for graph_id in range(X_train.shape[0]):
-    for node_id in range(graphs_train.number_of_graph_nodes[graph_id]):
-        if node_id == 0:
-            graphs_train.add_graph_node_edge(graph_id, "User", "Item", "UserItem")
-            
-        if node_id == 1:
-            graphs_train.add_graph_node_edge(graph_id, "Item", "Category", "ItemCategory")
-            graphs_train.add_graph_node_edge(graph_id, "Item", "User", "ItemUser")
-            
-        if node_id == 2:
-            graphs_train.add_graph_node_edge(graph_id, "Category", "Item", "CatrgoryItem")
-
-    graphs_train.add_graph_node_property(graph_id, "User", "U_" + str(X_train[graph_id][0]))
-    graphs_train.add_graph_node_property(graph_id, "Item", "I_" + str(X_train[graph_id][1]))
-    graphs_train.add_graph_node_property(graph_id, "Category", "C_" + str(X_train[graph_id][2]))
-graphs_train.encode()
-print("Training data produced")
-
-# Test data
-graphs_test = Graphs(X_test.shape[0], init_with=graphs_train)
-for graph_id in range(X_test.shape[0]):
-    graphs_test.set_number_of_graph_nodes(graph_id, number_of_nodes)
-graphs_test.prepare_node_configuration()
-for graph_id in range(X_test.shape[0]):
-    for node_id in range(graphs_test.number_of_graph_nodes[graph_id]):
-        number_of_edges = 2 if node_id > 0 and node_id < graphs_test.number_of_graph_nodes[graph_id]-1 else 1
-        if node_id == 0:
-            graphs_test.add_graph_node(graph_id, "User", number_of_edges)
-        elif node_id == 1:
-            graphs_test.add_graph_node(graph_id, "Item", number_of_edges)
-        else:
-            graphs_test.add_graph_node(graph_id, "Category", number_of_edges)
-graphs_test.prepare_edge_configuration()
-for graph_id in range(X_test.shape[0]):
-    for node_id in range(graphs_test.number_of_graph_nodes[graph_id]):
-        if node_id == 0:
-            graphs_test.add_graph_node_edge(graph_id, "User", "Item", "UserItem")
-            
-        if node_id == 1:
-            graphs_test.add_graph_node_edge(graph_id, "Item", "Category", "ItemCategory")
-            graphs_test.add_graph_node_edge(graph_id, "Item", "User", "ItemUser")
-            
-        if node_id == 2:
-            graphs_test.add_graph_node_edge(graph_id, "Category", "Item", "CatrgoryItem")
-
-    graphs_test.add_graph_node_property(graph_id, "User", "U_" + str(X_test[graph_id][0]))
-    graphs_test.add_graph_node_property(graph_id, "Item", "I_" + str(X_test[graph_id][1]))
-    graphs_test.add_graph_node_property(graph_id, "Category", "C_" + str(X_test[graph_id][2]))
-graphs_test.encode()
-print("Testing data produced")
-
-tm = MultiClassGraphTsetlinMachine(
-    args.number_of_clauses,
-    args.T,
-    args.s,
-    number_of_state_bits = args.number_of_state_bits,
-    depth=args.depth,
-    message_size=args.message_size,
-    message_bits=args.message_bits,
-    max_included_literals=args.max_included_literals,
-    double_hashing = args.double_hashing
-)
-
-for i in range(args.epochs):
-    start_training = time()
-    tm.fit(graphs_train, Y_train, epochs=1, incremental=True)
-    stop_training = time()
-
-    start_testing = time()
-    result_test = 100*(tm.predict(graphs_test) == Y_test).mean()
-    stop_testing = time()
-
-    result_train = 100*(tm.predict(graphs_train) == Y_train).mean()
-    print("%d %.2f %.2f %.2f %.2f" % (i, result_train, result_test, stop_training-start_training, stop_testing-start_testing))
-
-# weights = tm.get_state()[1].reshape(2, -1)
-# for i in range(tm.number_of_clauses):
-#     print("Clause #%d W:(%d %d)" % (i, weights[0,i], weights[1,i]), end=' ')
-#     l = []
-#     for k in range(args.hypervector_size * 2):
-#         if tm.ta_action(0, i, k):
-#             if k < args.hypervector_size:
-#                 l.append("x%d" % (k))
-#             else:
-#                 l.append("NOT x%d" % (k - args.hypervector_size))
-#     for k in range(args.message_size * 2):
-#         if tm.ta_action(1, i, k):
-#             if k < args.message_size:
-#                 l.append("c%d" % (k))
-#             else:
-#                 l.append("NOT c%d" % (k - args.message_size))
-#     print(" AND ".join(l))
-# print(graphs_test.hypervectors)
-# print(tm.hypervectors)
-# print(graphs_test.edge_type_id)
+if __name__ == "__main__":
+    main(default_args())
--- examples/applications/products_recommendation/tm_classifier.py
+++ examples/applications/products_recommendation/tm_classifier.py
@@ -1,60 +1,59 @@
-import logging
 import argparse
 from tmu.models.classification.vanilla_classifier import TMClassifier
 from tmu.tools import BenchmarkTimer
-from tmu.util.cuda_profiler import CudaProfiler
 import prepare_dataset
+import pandas as pd
+import os
 
-_LOGGER = logging.getLogger(__name__)
-
-def metrics(args):
-    return dict(
-        accuracy=[],
-        train_time=[],
-        test_time=[],
-        args=vars(args)
-    )
-
-def main(args):   
-    experiment_results = metrics(args)
-    data = prepare_dataset.aug_amazon_products()
+def main(args):
+    results = []
+    data = prepare_dataset.aug_amazon_products(noise_ratio = args.dataset_noise_ratio)
     x, y = prepare_dataset.construct_x_y(data)
     X_train, X_test, Y_train, Y_test = prepare_dataset.one_hot_encoding(x,y)
-    
     tm = TMClassifier(
         number_of_clauses=args.num_clauses,
         T=args.T,
         s=args.s,
         max_included_literals=args.max_included_literals,
         platform=args.platform,
-        weighted_clauses=args.weighted_clauses
+        weighted_clauses=args.weighted_clauses,
     )
-    _LOGGER.info(f"Running {TMClassifier} for {args.epochs}")
+        
     for epoch in range(args.epochs):
         benchmark_total = BenchmarkTimer(logger=None, text="Epoch Time")
         with benchmark_total:
             benchmark1 = BenchmarkTimer(logger=None, text="Training Time")
             with benchmark1:
-                res = tm.fit(
-                    X_train,
-                    Y_train,
-                )
-
-            experiment_results["train_time"].append(benchmark1.elapsed())
+                tm.fit(X_train, Y_train)
+            train_time = benchmark1.elapsed()
             benchmark2 = BenchmarkTimer(logger=None, text="Testing Time")
             with benchmark2:
-                result = 100 * (tm.predict(X_test) == Y_test).mean()
-                experiment_results["accuracy"].append(result)
-            experiment_results["test_time"].append(benchmark2.elapsed())
+                accuracy = 100 * (tm.predict(X_test) == Y_test).mean()
+            test_time = benchmark2.elapsed()
+        total_time = benchmark_total.elapsed()
+        
+        # Append results for each epoch
+    results.append({
+        "Algorithm": "TMClassifier",
+        "Noise_Ratio": args.dataset_noise_ratio,
+        "T": args.T,
+        "s": args.s,
+        "Max_Included_Literals": args.max_included_literals,
+        "Epochs": args.epochs,
+        "Platform": args.platform,
+        "Total_Time": total_time,
+        "Accuracy": accuracy,
+    })
 
-            _LOGGER.info(f"Epoch: {epoch + 1}, Accuracy: {result:.2f}, Training Time: {benchmark1.elapsed():.2f}s, "
-                         f"Testing Time: {benchmark2.elapsed():.2f}s")
-
-        if args.platform == "CUDA":
-            CudaProfiler().print_timings(benchmark=benchmark_total)
-
-    return experiment_results
-
+    # Save results to CSV
+    results_df = pd.DataFrame(results)
+    results_file = "experiment_results.csv"
+    if os.path.exists(results_file):
+        results_df.to_csv(results_file, mode='a', index=False, header=False)
+    else:
+        results_df.to_csv(results_file, index=False)
+    print(f"Results saved to {results_file}")
+    
 def default_args(**kwargs):
     parser = argparse.ArgumentParser()
     parser.add_argument("--num_clauses", default=2000, type=int)
@@ -64,6 +63,7 @@ def default_args(**kwargs):
     parser.add_argument("--platform", default="CPU_sparse", type=str, choices=["CPU", "CPU_sparse", "CUDA"])
     parser.add_argument("--weighted_clauses", default=True, type=bool)
     parser.add_argument("--epochs", default=10, type=int)
+    parser.add_argument("--dataset_noise_ratio", default=0.01, type=float)
     args = parser.parse_args()
     for key, value in kwargs.items():
         if key in args.__dict__:
@@ -71,5 +71,4 @@ def default_args(**kwargs):
     return args
 
 if __name__ == "__main__":
-    results = main(default_args())
-    _LOGGER.info(results)
+    main(default_args())
