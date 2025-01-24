1cb2ae9447584cb26146259420370e23833f7bae
Ole-Christoffer Granmo <ole.granmo@uia.no>
2025-01-24 11:08:11 +0100

    Update tm.py

--- GraphTsetlinMachine/tm.py
+++ GraphTsetlinMachine/tm.py
@@ -93,7 +93,7 @@ class CommonTsetlinMachine():
 			prime = prevprime(self.message_size)
 			for i in range(self.number_of_clauses):
 				self.hypervectors[i, 0] = i % (self.message_size)
-				self.hypervectors[i, 1] = (self.message_size) + prime - (i % prime)
+				self.hypervectors[i, 1] = prime - (i % prime)
 		else:
 			indexes = np.arange(self.message_size, dtype=np.uint32)
 			self.hypervectors = np.zeros((self.number_of_clauses, self.message_bits), dtype=np.uint32)
