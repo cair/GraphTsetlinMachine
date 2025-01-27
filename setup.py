b8f1909fd9fe19468cfcf3a7400147faa6a6c5eb
Ole-Christoffer Granmo <ole.granmo@uia.no>
2025-01-27 11:56:56 +0100

    Node types

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import *
 
 setup(
    name='GraphTsetlinMachine',
-   version='0.3.1',
+   version='0.3.2',
    author='Ole-Christoffer Granmo',
    author_email='ole.granmo@uia.no',
    url='https://github.com/cair/GraphTsetlinMachine/',
