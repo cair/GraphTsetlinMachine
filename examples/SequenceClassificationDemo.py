1c383933ded94021e2a962f8ecba83296b76f8c5
Ole-Christoffer Granmo <ole.granmo@uia.no>
2025-03-29 16:44:08 +0100

    Update

--- examples/SequenceClassificationDemo.py
+++ examples/SequenceClassificationDemo.py
@@ -101,8 +101,6 @@ for graph_id in range(args.number_of_examples):
 
 graphs_test.prepare_edge_configuration()
 
-Y_right = np.empty(args.number_of_examples, dtype=np.uint32)
-
 Y_test = np.empty(args.number_of_examples, dtype=np.uint32)
 for graph_id in range(args.number_of_examples):
     for node_id in range(graphs_test.number_of_graph_nodes[graph_id]):
@@ -117,9 +115,7 @@ for graph_id in range(args.number_of_examples):
             graphs_test.add_graph_node_edge(graph_id, node_id, destination_node_id, edge_type)
 
     Y_test[graph_id] = np.random.randint(args.number_of_classes)
-    Y_right[graph_id] = np.random.randint(2)
-    location = [Y_test[graph_id] + args.distance_from_edge, args.max_sequence_length - 1 - args.distance_from_edge]
-    node_id = location[Y_right[graph_id]]
+    node_id = np.random.choice([Y_test[graph_id] + args.distance_from_edge, args.max_sequence_length - 1 - args.distance_from_edge])
     for node_pos in range(Y_test[graph_id] + 1):
         graphs_test.add_graph_node_property(graph_id, node_id - node_pos, 'A')
 
@@ -149,9 +145,7 @@ for i in range(args.epochs):
     result_test = 100*(y_test_pred == Y_test).mean()
     stop_testing = time()
 
-    print("Left\n", confusion_matrix(Y_test[Y_right==0], y_test_pred[Y_right==0]))
-
-    print("Right\n", confusion_matrix(Y_test[Y_right==1], y_test_pred[Y_right==1]))
+    print("Test\n", confusion_matrix(Y_test, y_test_pred))
 
 
     y_train_pred = tm.predict(graphs_train)
