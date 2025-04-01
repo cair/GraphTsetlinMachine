64d13efc85a4558b620b678b41906e4e8049cf98
Ole-Christoffer Granmo <ole.granmo@uia.no>
2025-04-01 10:15:21 +0200

    Update

--- examples/SequenceClassificationDemo.py
+++ examples/SequenceClassificationDemo.py
@@ -171,18 +171,18 @@ for i in range(tm.number_of_clauses):
 
         for depth in range(1, args.depth):
             for k in range(tm.message_size * 2):
-                if k % 2 == 0:
+                if k % 2 == 1:
                     if tm.ta_action(depth, i, k):
                         if k < tm.message_size:
                             l.append("l%d:%d" % (depth, k // 2))
                         else:
                             l.append("NOT l%d:%d" % (depth, (k - tm.message_size) // 2))
-                    else:
-                        if tm.ta_action(depth, i, k):
-                            if k < tm.message_size:
-                                l.append("r%d:%d" % (depth, k // 2))
-                            else:
-                                l.append("NOT r%d:%d" % (depth, (k - tm.message_size) // 2))
+                else:
+                    if tm.ta_action(depth, i, k):
+                        if k < tm.message_size:
+                            l.append("r%d:%d" % (depth, k // 2))
+                        else:
+                            l.append("NOT r%d:%d" % (depth, (k - tm.message_size) // 2))
 
         print(" AND ".join(l))
 
