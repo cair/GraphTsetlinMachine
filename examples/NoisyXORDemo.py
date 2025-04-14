0910d0151456e5829e85561e9c3edb58994ff958
Ole-Christoffer Granmo <ole.granmo@uia.no>
2025-04-14 15:04:39 +0200

    Update

--- examples/NoisyXORDemo.py
+++ examples/NoisyXORDemo.py
@@ -149,7 +149,6 @@ for i in range(args.epochs):
 
 weights = tm.get_state()[1].reshape(2, -1)
 for b in range(tm.number_of_blocks):
-    print(b)
     print("Block #%d W:(%d %d)" % (b, weights[0,b], weights[1,b]))
 
     for i in range(b*(tm.number_of_clauses // tm.number_of_blocks), (b+1)*(tm.number_of_clauses // tm.number_of_blocks)):
