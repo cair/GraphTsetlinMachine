8c1f18c150d1a87cb793a1ff1ca9d3d86757e072
Ole-Christoffer Granmo <ole.granmo@uia.no>
2025-07-23 12:21:36 +0200

    Update

--- GraphTsetlinMachine/tm.py
+++ GraphTsetlinMachine/tm.py
@@ -729,10 +729,10 @@ class CommonTsetlinMachine():
 
 		return current_clause_node_output
 
-	def _fit(self, graphs, encoded_Y, epochs=100, incremental=False):
-		return
-		
+	def _fit(self, graphs, encoded_Y, epochs=100, incremental=False):		
 		self._init_fit(graphs, encoded_Y, incremental)
+
+		return
 
 		class_sum = np.zeros(self.number_of_outputs).astype(np.int32)
 		for epoch in range(epochs):
