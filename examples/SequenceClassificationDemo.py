b9ec7a9b319bbe80eaca62aeb8de92f79a2a0c29
Ole-Christoffer Granmo <ole.granmo@uia.no>
2025-07-24 11:34:11 +0200

    Update

--- examples/SequenceClassificationDemo.py
+++ examples/SequenceClassificationDemo.py
@@ -147,7 +147,7 @@ for i in range(args.epochs):
 
     print("%d %.2f %.2f %.2f %.2f" % (i, result_train, result_test, stop_training-start_training, stop_testing-start_testing))
 
-weights = tm.get_state()[1].reshape(2, -1)
+weights = tm.get_state()[1].reshape(tm.number_of_outputs, -1)
 for i in range(tm.number_of_clauses):
         output_weights = []
         for j in range(tm.number_of_outputs):
