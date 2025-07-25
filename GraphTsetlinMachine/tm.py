e020ca95be7dbd154a74bff161c76098acf8c503
Ole-Christoffer Granmo <ole.granmo@uia.no>
2025-07-25 10:51:03 +0200

    Update

--- GraphTsetlinMachine/tm.py
+++ GraphTsetlinMachine/tm.py
@@ -422,9 +422,8 @@ class CommonTsetlinMachine():
 
 		self.number_of_features = graphs.hypervector_size
 		self.number_of_literals = self.number_of_features*2
-		self.number_of_ta_chunks = int((self.number_of_literals-1)//32 + 1)
-
-		if self.one_hot_encoding and not self.attention:
+		self.number_of_ta_chunks
+		 and not self.attention:
 			self.message_size = self.number_of_clauses * len(graphs.edge_type_id)
 			for i in range(self.number_of_clauses):
 				self.hypervectors[i, 0] = i * len(graphs.edge_type_id)
@@ -738,6 +737,7 @@ class CommonTsetlinMachine():
 		class_sum = np.zeros(self.number_of_outputs).astype(np.int32)
 		for epoch in range(epochs):
 			for e in range(graphs.number_of_graphs):
+				graphs.print_graph(e)
 				class_sum[:] = 0
 				cuda.memcpy_htod(self.class_sum_gpu, class_sum)
 
