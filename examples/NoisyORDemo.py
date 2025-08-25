7c3ea27a3632d5b2967a52fb953ac28e3d052bb7
Ole-Christoffer Granmo <ole.granmo@uia.no>
2025-08-25 17:44:25 +0200

    Update

--- examples/NoisyORDemo.py
+++ examples/NoisyORDemo.py
@@ -71,7 +71,6 @@ for graph_id in range(args.number_of_examples):
     for symbol in x:
         graphs_train.add_graph_node_property(graph_id, 'Node', symbol)
 
-        print(Y_train[graph_id])
     if np.random.rand() <= args.noise:
         Y_train[graph_id] = 1 - Y_train[graph_id]
 
@@ -108,7 +107,7 @@ for graph_id in range(args.number_of_examples):
         x = []
  
     for symbol in x:
-        graphs_test.add_graph_node_property(graph_id, 'Node 1', symbol)
+        graphs_test.add_graph_node_property(graph_id, 'Node', symbol)
 
 graphs_test.encode()
 
